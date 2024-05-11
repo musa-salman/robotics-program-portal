ca084820c9bd4385c83046f2385f449af0a07c24
Mosa Salman <55544612+mosa-salman@users.noreply.github.com>
2024-05-11 22:33:08 +0300

    refactor: Separate Firebase SDK initialization and configuration into a dedicated file

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,35 +4,16 @@ import dotenv from 'dotenv';
 import connectDB from './config/db';
 import { healthRoutes } from './routes/healthRoutes';
 import { notFound, errorHandler } from './middleware/errorMiddleware';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 dotenv.config();
 
 // Connect to MongoDB
 connectDB();
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
-};
-
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
-
+// Initialize Express
 const app: Express = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
