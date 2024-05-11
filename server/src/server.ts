8fda15af5afd1aba4a18b4fcc21a4f3de69d8e27
Mosa Salman <55544612+mosa-salman@users.noreply.github.com>
2024-05-11 04:29:57 +0300

    chore: Update Firebase SDK initialization and configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,23 +8,23 @@ import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+dotenv.config();
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDl2TlgNRuQD-4E1yNN3k_3nin7B6RhMjk",
-  authDomain: "pico-7a9d2.firebaseapp.com",
-  projectId: "pico-7a9d2",
-  storageBucket: "pico-7a9d2.appspot.com",
-  messagingSenderId: "673223833904",
-  appId: "1:673223833904:web:cb1397e6b218bbae2a70f5",
-  measurementId: "G-5ZH9CK0CSH"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const analytics = getAnalytics(firebaseApp);
-
-dotenv.config();
 
 const app: Express = express();
 
