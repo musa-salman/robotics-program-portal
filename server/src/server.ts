4a66c30089dadd5a0a00e673da28f6524c8316cc
Mosa Salman <55544612+mosa-salman@users.noreply.github.com>
2024-05-11 03:28:53 +0300

    chore: Add Firebase SDK initialization

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,27 @@ import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDl2TlgNRuQD-4E1yNN3k_3nin7B6RhMjk",
+  authDomain: "pico-7a9d2.firebaseapp.com",
+  projectId: "pico-7a9d2",
+  storageBucket: "pico-7a9d2.appspot.com",
+  messagingSenderId: "673223833904",
+  appId: "1:673223833904:web:cb1397e6b218bbae2a70f5",
+  measurementId: "G-5ZH9CK0CSH"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 dotenv.config();
 
