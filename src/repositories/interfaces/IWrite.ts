61e4fe99b3ad3502fb359b929d2168e455ceb519
Mosa Salman <mosa.salman.g@gmail.com>
2024-05-24 11:21:21 +0300

    chore: Update IWrite interface to return void for update and delete methods

--- src/repositories/interfaces/IWrite.ts
+++ src/repositories/interfaces/IWrite.ts
@@ -2,6 +2,6 @@ import { DocumentReference } from "firebase/firestore";
 
 export interface IWrite<T> {
     create(item: T): Promise<DocumentReference>;
-    update(id: string, item: T): Promise<boolean>;
-    delete(id: string): Promise<boolean>;
+    update(id: string, item: T): Promise<void>;
+    delete(id: string): Promise<void>;
 }
