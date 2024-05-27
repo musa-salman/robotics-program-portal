914fdfee62b88546b40b9e07605424c3d6210794
Mosa Salman <mosa.salman.g@gmail.com>
2024-05-27 17:46:37 +0300

    refactor: Update Container component to use useContext hook for studyMaterialRepository

--- src/study-material/Container.tsx
+++ src/study-material/Container.tsx
@@ -1,41 +1,41 @@
 import Card from 'react-bootstrap/Card';
 import StudyMaterials from './StudyMaterials'
-import  { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import "./Container.css"
 import { StudyMaterialContext } from './StudyMaterialContext';
 import { StudyMaterial } from '../upload-file/StudyMaterial';
 
-function Container() {  
-
+function Container() {
     const [studyMaterials, setStudyMaterials] = useState<StudyMaterial[] | null>(null);
     const studyMaterialRepository = useContext(StudyMaterialContext);
 
     useEffect(() => {
         const getStudyMaterials = async () => {
-            setStudyMaterials( await studyMaterialRepository.find());
+            setStudyMaterials(await studyMaterialRepository.find());
             console.log(studyMaterials);
         };
 
-       if (studyMaterials === null)getStudyMaterials()
-    }, [studyMaterials]); 
+        if (studyMaterials === null) getStudyMaterials()
+
+    }, [studyMaterials]);
 
 
     return (
-<>
-<Card className="primary" >
-        <Card.Header>Header</Card.Header>
-        <br></br>
-        <Card.Body className='body'>
-            <br></br>
-            <div className="study-materials-container">    
-            {/* {studyMaterials.map(docId => (
+        <>
+            <Card className="primary" >
+                <Card.Header>Header</Card.Header>
+                <br></br>
+                <Card.Body className='body'>
+                    <br></br>
+                    <div className="study-materials-container">
+                        {/* {studyMaterials.map(docId => (
                         <StudyMaterials key={docId} docId={docId} />
                     ))} */}
-            </div>    
-        </Card.Body>
-      </Card>
-</>
- );
+                    </div>
+                </Card.Body>
+            </Card>
+        </>
+    );
 }
 
 
