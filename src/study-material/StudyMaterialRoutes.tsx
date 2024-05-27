8424557378b5cbb82cca5eb6af44e66f71429a03
Mosa Salman <mosa.salman.g@gmail.com>
2024-05-27 17:50:20 +0300

    refactor: Rename Container component to StudyMaterialContainer in StudyMaterialRoutes.tsx

--- src/study-material/StudyMaterialRoutes.tsx
+++ src/study-material/StudyMaterialRoutes.tsx
@@ -1,10 +1,10 @@
 import { Route } from "react-router-dom"
-import Container from "./Container"
+import StudyMaterialContainer from "./StudyMaterialContainer"
 
 export const useStudyMaterialRoutes = () => {
     return (
         <>
-            <Route path="/study-material" element={<Container />} />
+            <Route path="/study-material" element={<StudyMaterialContainer />} />
         </>
     )
 }
