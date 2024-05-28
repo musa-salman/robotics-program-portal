eb9c2c869d755bbc86d045d2248b67158c2a52e9
Mosa Salman <mosa.salman.g@gmail.com>
2024-05-28 16:54:53 +0300

    refactor: Clean up StudyMaterialContainer component

--- src/study-material/StudyMaterialContainer.tsx
+++ src/study-material/StudyMaterialContainer.tsx
@@ -1,56 +1,56 @@
 import Card from 'react-bootstrap/Card';
 import StudyMaterials from './StudyMaterials'
-import  { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import "./StudyMaterialContainer.css"
 import { StudyMaterialContext } from './StudyMaterialContext';
 import { StudyMaterial } from './StudyMaterial';
 
 
-function StudyMaterialContainer() {  
+function StudyMaterialContainer() {
 
     const [studyMaterials, setStudyMaterials] = useState<StudyMaterial[] | null>(null);
     const studyMaterialRepository = useContext(StudyMaterialContext);
 
     useEffect(() => {
         const getStudyMaterials = async () => {
-            setStudyMaterials( await studyMaterialRepository.find());    
+            setStudyMaterials(await studyMaterialRepository.find());
         };
 
-       if (studyMaterials === null)getStudyMaterials()
-    }, [studyMaterials]); 
+        if (studyMaterials === null) getStudyMaterials();
+    }, [studyMaterials]);
 
-    
+
     console.log(studyMaterials);
- 
-    const categories = studyMaterials?.map((s) =>s.category).filter((item, index , arr)=> arr.indexOf(item) === index);
+
+    const categories = studyMaterials?.map((s) => s.category).filter((item, index, arr) => arr.indexOf(item) === index);
     return (
-<>
+        <>
 
-{(categories || []).map(category => (
+            {(categories || []).map(category => (
 
-<Card className="primary" >
+                <Card className="primary" >
 
-        <Card.Header className="Card-Header"> 
-        <div key={category}>
-          <h2>{category}</h2>
-        </div>
-        <button className="add-button"></button>
-        </Card.Header>
-        <br></br>
-        <Card.Body className='body'>
-            <br></br>
-            <div className="study-materials-container"> 
+                    <Card.Header className="Card-Header">
+                        <div key={category}>
+                            <h2>{category}</h2>
+                        </div>
+                        <button className="add-button"></button>
+                    </Card.Header>
+                    <br></br>
+                    <Card.Body className='body'>
+                        <br></br>
+                        <div className="study-materials-container">
 
-            {(studyMaterials || [] ).filter((s) => s.category === category).map(studyMaterial=> (
-                        <StudyMaterials key={studyMaterial.id}  studyMaterial={studyMaterial} />
-                    ))}
-            </div>    
-        </Card.Body>
-      </Card>
-))}
-</>
- );
+                            {(studyMaterials || []).filter((s) => s.category === category).map(studyMaterial => (
+                                <StudyMaterials key={studyMaterial.id} studyMaterial={studyMaterial} />
+                            ))}
+                        </div>
+                    </Card.Body>
+                </Card>
+            ))}
+        </>
+    );
 }
 
 
-export default  StudyMaterialContainer;
+export default StudyMaterialContainer;
