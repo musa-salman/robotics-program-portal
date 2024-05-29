a9d7d60e807ebe1beb324348c932fc69a8b8c73d
Mosa Salman <mosa.salman.g@gmail.com>
2024-05-29 23:47:59 +0300

    chore: Update Prettier configuration and scripts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,17 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
-      { allowConstantExport: true },
+      { allowConstantExport: true }
     ],
-  },
-}
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': 'off'
+  }
+};
