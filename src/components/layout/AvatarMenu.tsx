5036769c574c7b38abc03f5cb96844d2912c014c
Mosa Salman <mosa.salman.g@gmail.com>
2024-06-04 11:01:10 +0300

    feat: Add AvatarMenu component for user profile menu

---
+++ src/components/layout/AvatarMenu.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { deepOrange } from '@mui/material/colors';
+import LogoutButton from '../../authentication/components/Logout';
+import './AvatarMenu.css'; // Import CSS file here
+import RoleBasedAccessControl from '../../authentication/components/RoleBasedAccessControl';
+import { ALLOW_AUTHED_ROLES } from '../../authentication/components/Roles';
+import { auth } from '../../firebase';
+import { Person } from '@mui/icons-material';
+
+const AvatarMenu = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleAvatarClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <div onClick={handleAvatarClick} className="avatar-menu-container">
+        <Avatar sx={{ bgcolor: deepOrange[500] }}>
+          {auth.currentUser?.photoURL ? <img src={auth.currentUser?.photoURL} alt="avatar" /> : <Person />}
+        </Avatar>
+        {auth.currentUser?.displayName && <span className="avatar-name">{auth.currentUser?.displayName}</span>}
+      </div>
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleMenuClose}
+        anchorOrigin={{
+          vertical: 'bottom', // Place the menu below the avatar
+          horizontal: 'right'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right'
+        }}>
+        <div className="avatar-menu">
+          <RoleBasedAccessControl
+            allowedRoles={ALLOW_AUTHED_ROLES}
+            unauthorizedUnauthenticatedComponent={
+              <MenuItem onClick={handleMenuClose}>
+                <Link to="/login" className="login-button">
+                  התחברות
+                </Link>
+              </MenuItem>
+            }>
+            <MenuItem onClick={handleMenuClose}>
+              <Link to="/profile" className="profile-button">
+                פרופיל
+              </Link>
+            </MenuItem>
+            <MenuItem onClick={handleMenuClose}>
+              <LogoutButton />
+            </MenuItem>
+          </RoleBasedAccessControl>
+        </div>
+      </Menu>
+    </>
+  );
+};
+
+export default AvatarMenu;
