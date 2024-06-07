64722ca81221d933a28410d04c89a88eff470728
Mosa Salman <mosa.salman.g@gmail.com>
2024-06-07 10:31:45 +0300

    chore: Update npm dependencies for stylis, and stylis-plugin-rtl

--- src/main.tsx
+++ src/main.tsx
@@ -10,25 +10,45 @@ import StudyMaterialProvider from './study-material/StudyMaterialContext.tsx';
 import StorageServiceProvider from './storage-service/StorageContext.tsx';
 import EventProvider from './events/EventContext.tsx';
 import { AuthProvider } from './authentication/AuthContext.tsx';
+import StudentProvider from './students_management/StudentContext.tsx';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import rtlPlugin from 'stylis-plugin-rtl';
+import createTheme from '@mui/material/styles/createTheme';
+
+const cacheRtl = createCache({
+  key: 'muirtl',
+  stylisPlugins: [rtlPlugin]
+});
+
+const theme = createTheme({
+  direction: 'rtl'
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider dir="rtl">
-      <BrowserRouter>
-        <UserProvider>
-          <CategoryProvider>
-            <StudyMaterialProvider>
-              <EventProvider>
-                <StorageServiceProvider>
-                  <AuthProvider>
-                    <App />
-                  </AuthProvider>
-                </StorageServiceProvider>
-              </EventProvider>
-            </StudyMaterialProvider>
-          </CategoryProvider>
-        </UserProvider>
-      </BrowserRouter>
+      <CacheProvider value={cacheRtl}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <UserProvider>
+              <CategoryProvider>
+                <StudyMaterialProvider>
+                  <EventProvider>
+                    <StudentProvider>
+                      <StorageServiceProvider>
+                        <AuthProvider>
+                          <App />
+                        </AuthProvider>
+                      </StorageServiceProvider>
+                    </StudentProvider>
+                  </EventProvider>
+                </StudyMaterialProvider>
+              </CategoryProvider>
+            </UserProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </CacheProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
