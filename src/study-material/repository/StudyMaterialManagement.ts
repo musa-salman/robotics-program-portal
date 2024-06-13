3c0bed8530cb508da03aa5bacd5c58c040098d4f
Mosa Salman <mosa.salman.g@gmail.com>
2024-06-13 14:54:21 +0300

    docs: add docs to study material management.

--- src/study-material/repository/StudyMaterialManagement.ts
+++ src/study-material/repository/StudyMaterialManagement.ts
@@ -16,63 +16,93 @@ export interface IUnitOfWork {
   moveMaterialToCategory(studyMaterial: StudyMaterial, oldCategoryId: string, newCategoryId: string): Promise<void>;
 }
 
+/**
+ * Represents the management class for study materials.
+ * Implements the IUnitOfWork interface.
+ */
 export class StudyMaterialManagement implements IUnitOfWork {
-  categoryRepository: CategoryRepository;
-  studyMaterialRepositories: Map<string, StudyMaterialRepository>;
+  readonly categoryRepository: CategoryRepository;
+  readonly studyMaterialRepositories: Map<string, StudyMaterialRepository>;
 
   constructor() {
     this.categoryRepository = new CategoryRepository();
     this.studyMaterialRepositories = new Map<string, StudyMaterialRepository>();
   }
 
-  getStudyMaterialRepository(categoryId: string): StudyMaterialRepository {
-    let studyMaterialRepository = this.studyMaterialRepositories.get(categoryId);
-    if (!studyMaterialRepository) {
-      studyMaterialRepository = new StudyMaterialRepository(categoryId);
-      this.studyMaterialRepositories.set(categoryId, studyMaterialRepository);
-    }
-    return studyMaterialRepository;
+  /**
+   * Retrieves study materials grouped by category.
+   * @returns A Promise that resolves to a Map containing study materials grouped by category.
+   */
+  async getStudyMaterialsByCategory(): Promise<Map<string, StudyMaterial[]>> {
+    let studyMaterials = new Map<string, StudyMaterial[]>();
+
+    this.categoryRepository.find().then((categories) => {
+      categories.forEach(async (category) => {
+        this.findStudyMaterialInCategory(category.id).then((materials) => studyMaterials.set(category.id, materials));
+      });
+    });
+
+    return studyMaterials;
   }
 
+  /**
+   * Adds a study material to a category.
+   *
+   * @param categoryId - The ID of the category to add the study material to.
+   * @param data - The study material data to add.
+   * @returns A promise that resolves to the reference of the created study material document.
+   */
   async addStudyMaterialToCategory(
     categoryId: string,
     data: StudyMaterial
   ): Promise<DocumentReference<StudyMaterial, DocumentData>> {
-    const studyMaterialRepository = await this.getStudyMaterialRepository(categoryId);
-    return studyMaterialRepository.create(data);
+    return this.getStudyMaterialRepository(categoryId).create(data);
   }
 
+  /**
+   * Deletes a study material from a category.
+   *
+   * @param categoryId - The ID of the category.
+   * @param materialId - The ID of the study material to delete.
+   * @returns A Promise that resolves to void.
+   */
   async deleteStudyMaterialFromCategory(categoryId: string, materialId: string): Promise<void> {
-    const studyMaterialRepository = await this.getStudyMaterialRepository(categoryId);
-    await studyMaterialRepository.delete(materialId);
+    await this.getStudyMaterialRepository(categoryId).delete(materialId);
   }
 
+  /**
+   * Updates a study material in a specific category.
+   *
+   * @param categoryId - The ID of the category.
+   * @param materialId - The ID of the material to update.
+   * @param data - The partial data of the study material to update.
+   * @returns A promise that resolves to void.
+   */
   async updateStudyMaterialInCategory(
     categoryId: string,
     materialId: string,
     data: Partial<StudyMaterial>
   ): Promise<void> {
-    const studyMaterialRepository = await this.getStudyMaterialRepository(categoryId);
-    await studyMaterialRepository.update(materialId, data);
+    return this.getStudyMaterialRepository(categoryId).update(materialId, data);
   }
 
+  /**
+   * Finds study materials in a specific category.
+   * @param categoryId - The ID of the category to search in.
+   * @returns A promise that resolves to an array of StudyMaterial objects.
+   */
   async findStudyMaterialInCategory(categoryId: string): Promise<StudyMaterial[]> {
-    const studyMaterialRepository = await this.getStudyMaterialRepository(categoryId);
-    return studyMaterialRepository.find();
+    return this.getStudyMaterialRepository(categoryId).find();
   }
 
-  async getStudyMaterialsByCategory(): Promise<Map<string, StudyMaterial[]>> {
-    {
-      let studyMaterials = new Map<string, StudyMaterial[]>();
-      const categories = await this.categoryRepository.find();
-      for (const category of categories) {
-        const materials = await this.findStudyMaterialInCategory(category.id);
-        studyMaterials.set(category.id, materials);
-      }
-      return studyMaterials;
-    }
-  }
-
+  /**
+   * Moves a study material from one category to another.
+   *
+   * @param studyMaterial - The study material to be moved.
+   * @param oldCategoryId - The ID of the old category.
+   * @param newCategoryId - The ID of the new category.
+   * @returns A promise that resolves when the study material has been moved successfully.
+   */
   async moveMaterialToCategory(
     studyMaterial: StudyMaterial,
     oldCategoryId: string,
@@ -90,5 +120,21 @@ export class StudyMaterialManagement implements IUnitOfWork {
 
     batch.delete(oldDocRef).set(newDocRef, remainingProperties);
     return batch.commit();
+  }
+
+  /**
+   * Retrieves the study material repository for the specified category ID.
+   * If the repository does not exist, a new one is created and stored.
+   *
+   * @param categoryId - The ID of the category.
+   * @returns The study material repository for the specified category ID.
+   */
+  private getStudyMaterialRepository(categoryId: string): StudyMaterialRepository {
+    let studyMaterialRepository = this.studyMaterialRepositories.get(categoryId);
+    if (!studyMaterialRepository) {
+      studyMaterialRepository = new StudyMaterialRepository(categoryId);
+      this.studyMaterialRepositories.set(categoryId, studyMaterialRepository);
+    }
+    return studyMaterialRepository;
   }
 }
