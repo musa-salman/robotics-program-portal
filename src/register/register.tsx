dee27c8fd7e9b09e89498027638ceda17dd84e09
ertugrul2002 <mohamaddkdk2002@gmail.com>
2024-06-13 23:17:37 +0300

    feat: saving

--- src/register/register.tsx
+++ src/register/register.tsx
@@ -10,16 +10,77 @@ import Page1Component from './Page1';
 import Page2Component from './page2';
 import Page3Component from './Page3';
 import Page4Component from './Page4';
+import { useState } from 'react';
+import { Register } from './Register';
 
 
 const steps = ['על המתחם החדש', 'פרטים אישיים', 'פרטים בית הספר','שאלות אחרונות'];
 
-const pages =[<Page1Component />,<Page2Component />,<Page3Component />,<Page4Component />];
+
+  
 
 const RegisterComponent =  () => {
-    const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set<number>());
+  const [activeStep, setActiveStep] = useState(0);
+  const [skipped, setSkipped] = useState(new Set<number>());
+  const [page1Register,setPage1Register]=useState<Page2Register>({
+    studentFirstName: '',
+    studentLastName: '',
+    studentPhone: '',
+    parentPhone: '',
+    studentId: '',
+    studentEmail: '',
+    parentEmail: '',
+    studentAddress: ''
+  })
+  const [register,setRegister]=useState<Register >({
+    id: '',
+    studentFirstName: '',
+    studentLastName: '',
+    studentPhone: '',
+    parentPhone: '',
+    studentId: '',
+    studentEmail: '',
+    parentEmail: '',
+    studentAddress: '',
+    studentSchool: '',
+    studyUnitsMajor: '',
+    numStudyUnitsMath: '',
+    hearAboutUs: '',
+    otherQuestions: ''
 
+
+  });
+
+  // const handleChange = (event:any) => {
+  //   const { name, value } = event.target;
+  //   setRegister((prevData) => ({ ...prevData, [name]: value }));
+  //   console.log(register);
+  // };
+
+  // const handleSubmit = (event:any) => {
+  //   event.preventDefault();
+  //   // Handle form submission logic
+  //   // console.log(formData);
+  // };
+
+  const pages =[
+    {
+      inputs:'',
+      page:<Page1Component />,
+    },
+    {
+      inputs:'',
+      page:<Page2Component setRegister={setPage1Register} />,
+    },
+    {
+      inputs:'',
+      page:<Page3Component setRegister={setRegister}/>,
+    },
+    {
+      inputs:'',
+      page:<Page4Component />,
+    },
+  ];
 
   const isStepSkipped = (step: number) => {
     return skipped.has(step);
@@ -30,17 +91,30 @@ const RegisterComponent =  () => {
     if (isStepSkipped(activeStep)) {
       newSkipped = new Set(newSkipped.values());
       newSkipped.delete(activeStep);
+      
     }
-
+    
+    console.log(register);
+    setRegister((prevData) => ({ ...prevData, 
+      studentFirstName: page1Register.studentFirstName,
+      studentLastName: page1Register.studentLastName,
+      studentPhone:page1Register.studentPhone,
+      parentPhone:page1Register.parentPhone,
+      studentId:page1Register.studentId,
+      studentEmail: page1Register.studentEmail,
+      parentEmail: page1Register.parentEmail,
+      studentAddress: page1Register.studentAddress
+      
+    }));
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
     setSkipped(newSkipped);
   };
 
+  
+
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-
-
 
   const handleReset = () => {
     setActiveStep(0);
@@ -77,7 +151,7 @@ const RegisterComponent =  () => {
                 </React.Fragment>
             ) : (
                 <React.Fragment>
-                 <Typography>{pages[activeStep]}</Typography>
+                 <Typography>{pages[activeStep].page}</Typography>
 
                 <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
                     <Button
