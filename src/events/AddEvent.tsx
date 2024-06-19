ddf8501d044e8c811598f30cb074fec849629ce3
Anas_Jamjoum <anas13672@gmail.com>
2024-06-19 22:50:35 +0300

    refactor: Update EmptyEventCard component to use AddEvent for adding events

--- src/events/AddEvent.tsx
+++ src/events/AddEvent.tsx
@@ -1,83 +1,185 @@
 // AddEvent.tsx
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import React, { useContext, useState } from 'react';
+import { Form, Button, Modal } from 'react-bootstrap';
 import { EventProps } from './EventCard';
+import { IEvent } from './repository/Event';
+import { eventManagerContext } from './repository/EventManagerContext';
+import { StorageServiceContext } from '../storage-service/StorageContext';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import AddIcon from '@mui/icons-material/Add';
 
-type AddEventProps = {
-  setFormData: React.Dispatch<React.SetStateAction<EventProps>>;
-  setFile: React.Dispatch<React.SetStateAction<File | null>>;
-  handleSaveAdd: (event: React.FormEvent<HTMLFormElement>) => void;
-  handleCloseAddEvent: () => void;
-};
+interface AddEventProps {
+  addEvent: (event: EventProps) => void;
+}
 
-export function AddEvent({ setFormData, setFile, handleSaveAdd, handleCloseAddEvent }: AddEventProps) {
-  const MAX_CHARS_Title = 17; // Set the maximum number of characters allowed
-  const MAX_CHARS_Details = 100; // Set the maximum number of characters allowed
+const AddEvent: React.FC<AddEventProps> = ({ addEvent }) => {
+  const [file, setFile] = useState<File | null>(null);
+  const [showModalAddEvent, setShowModalAddEvent] = useState(false);
+  const [_uploadProgress, setUploadProgress] = useState(0);
 
-  const handleDetailsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    if (value.length <= MAX_CHARS_Details) {
-      setFormData((prevState: any) => ({ ...prevState, details: value }));
+  const handleCloseAddEvent = () => setShowModalAddEvent(false);
+  const handleShowAddEvent = () => setShowModalAddEvent(true);
+
+  const eventManager = useContext(eventManagerContext);
+  const eventRepository = eventManager.eventRepository;
+  const storageService = useContext(StorageServiceContext);
+
+  const [formData, setFormData] = useState<EventProps>({
+    date: new Date(), // Provide initial value for date
+    title: '', // Provide initial value for title
+    details: '', // Provide initial value for details
+    image:
+      'https://firebasestorage.googleapis.com/v0/b/pico-7a9d2.appspot.com/o/event-img%2FRobtics.png?alt=media&token=ebd02a49-3e7a-4165-8580-825a2d5a0a5d', // Provide initial value for image
+    onEventDelete: (_id: string) => {}, // Change the parameter type from '_id: string' to 'id: number'
+    onEventEdit: (_event: EventProps) => {},
+    id: '' // Provide initial value for id
+  });
+
+  const handleAddEvent = () => {
+    handleShowAddEvent();
+  };
+
+  const handleSaveAdd = () => {
+    handleAdd();
+    setShowModalAddEvent(false);
+  };
+
+  const event: IEvent = {
+    date: new Date(formData.date),
+    title: formData.title,
+    details: formData.details,
+    imageURL: formData.image,
+    id: ''
+  };
+
+  async function handleAdd() {
+    handleShowAddEvent();
+    const docRef = await eventRepository.create(event);
+    event.id = docRef.id;
+    formData.id = docRef.id;
+    if (file) {
+      await storageService.upload(
+        file,
+        '/event-img/' + docRef.id,
+        setUploadProgress,
+        (_error) => {},
+        () => {
+          const storage = getStorage();
+          const filePath = '/event-img/' + docRef.id;
+          // Get the download URL
+          getDownloadURL(ref(storage, filePath)).then((url) => {
+            event.imageURL = url;
+            formData.image = url;
+            eventRepository.update(docRef.id, event);
+            addEvent(formData);
+          });
+        }
+      );
+    } else {
+      addEvent(formData);
     }
-  };
+  }
 
-  const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const value = e.target.value;
-    if (value.length <= MAX_CHARS_Title) {
-      setFormData((prevState: any) => ({ ...prevState, title: value }));
-    }
-  };
+  function addWindow() {
+    return (
+      <>
+        <Modal show={showModalAddEvent} onHide={handleCloseAddEvent} animation={false} style={{ display: 'center' }}>
+          <Modal.Header closeButton>
+            <Modal.Title>הוסף אירוע</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{addForm()}</Modal.Body>
+          <Modal.Footer></Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 
-  const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData((prevState: any) => ({ ...prevState, date: e.target.valueAsDate! }));
-  };
+  function addForm() {
+    const MAX_CHARS_Title = 17; // Set the maximum number of characters allowed
+    const MAX_CHARS_Details = 100; // Set the maximum number of characters allowed
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement & { files: FileList }>) => {
-    setFormData((prevState: any) => ({ ...prevState, image: e.target.files?.[0] }));
-    setFile(e.target.files?.[0] || null); // Provide a default value of null for the file state variable
-  };
+    const handleDetailsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const value = e.target.value;
+      if (value.length <= MAX_CHARS_Details) {
+        setFormData((prevState) => ({ ...prevState, details: value }));
+      }
+    };
+
+    const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      const value = e.target.value;
+      if (value.length <= MAX_CHARS_Title) {
+        setFormData((prevState) => ({ ...prevState, title: value }));
+      }
+    };
+
+    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFormData((prevState) => ({ ...prevState, date: e.target.valueAsDate! }));
+    };
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFormData((prevState) => ({ ...prevState, image: formData.image }));
+      setFile(e.target.files?.[0] || null); // Provide a default value of null for the file state variable
+    };
+
+    return (
+      <Form onSubmit={handleSaveAdd}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>כותרת</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="שם אירוע"
+            onChange={handleTitleChange}
+            maxLength={MAX_CHARS_Title} // Set the maximum length of the textarea
+          />
+          <small>
+            {formData.title.length}/{MAX_CHARS_Title} אותיות
+          </small>{' '}
+          {/* Display the character count */}
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>תאריך</Form.Label>
+          <Form.Control required type="date" placeholder="יום /חודש /שנה" onChange={handleDateChange} />
+        </Form.Group>
+        <Form.Group controlId="formFile" className="mb-3">
+          <Form.Label>העלאת תמונה</Form.Label>
+          <Form.Control type="file" accept="image/*" onChange={handleImageChange} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Label>פרטים</Form.Label>
+          <Form.Control
+            required
+            as="textarea"
+            rows={3}
+            placeholder="פרטי האירוע"
+            onChange={handleDetailsChange}
+            maxLength={MAX_CHARS_Details} // Set the maximum length of the textarea
+          />
+          <small>
+            {formData.details.length}/{MAX_CHARS_Details} אותיות
+          </small>{' '}
+          {/* Display the character count */}
+        </Form.Group>
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <Button variant="secondary" onClick={handleCloseAddEvent}>
+            סגור
+          </Button>
+          <Button variant="primary" type="submit">
+            הוסף
+          </Button>
+        </div>
+      </Form>
+    );
+  }
 
   return (
-    <Form onSubmit={handleSaveAdd}>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>כותרת</Form.Label>
-        <Form.Control
-          required
-          type="text"
-          placeholder="שם אירוע"
-          onChange={handleTitleChange}
-          maxLength={MAX_CHARS_Title} // Set the maximum length of the textarea
-        />
-        {/* Display the character count */}
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>תאריך</Form.Label>
-        <Form.Control required type="date" placeholder="יום /חודש /שנה" onChange={handleDateChange} />
-      </Form.Group>
-      <Form.Group controlId="formFile" className="mb-3">
-        <Form.Label>העלאת תמונה</Form.Label>
-        <Form.Control type="file" accept="image/*" onChange={handleImageChange} />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>פרטים</Form.Label>
-        <Form.Control
-          required
-          as="textarea"
-          rows={3}
-          placeholder="פרטי האירוע"
-          onChange={handleDetailsChange}
-          maxLength={MAX_CHARS_Details} // Set the maximum length of the textarea
-        />
-        {/* Display the character count */}
-      </Form.Group>
-      <div style={{ display: 'flex', gap: '10px' }}>
-        <Button variant="secondary" onClick={handleCloseAddEvent}>
-          סגור
-        </Button>
-        <Button variant="primary" type="submit">
-          הוסף
-        </Button>
-      </div>
-    </Form>
+    <>
+      <Button className="add-button" onClick={handleAddEvent}>
+        <AddIcon />
+      </Button>
+      {addWindow()}
+    </>
   );
-}
+};
+
+export default AddEvent;
--- src/events/EventContainer.tsx
+++ src/events/EventContainer.tsx
@@ -6,9 +6,9 @@ import { IEvent } from './repository/Event';
 import { StorageServiceContext } from '../storage-service/StorageContext';
 import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 import { CircularProgress, Box } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
 import './EventContainer.css';
 import EmptyEventCard from './EmptyEventCard';
+import AddEvent from './AddEvent';
 
 type EventContainer = {
   eventsProps: EventProps[];
@@ -16,10 +16,6 @@ type EventContainer = {
 
 const EventContainer = () => {
   const [events, setEvents] = useState<EventProps[] | null>(null);
-  const [showModalAddEvent, setShowModalAddEvent] = useState(false);
-  const handleCloseAddEvent = () => setShowModalAddEvent(false);
-  const handleShowAddEvent = () => setShowModalAddEvent(true);
-  const [file, setFile] = useState<File | null>(null);
   const [render, setRender] = useState(0);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [_uploadProgress, setUploadProgress] = useState(0);
@@ -48,6 +44,10 @@ const EventContainer = () => {
     if (events === null) getEvents();
   }, [events]);
 
+  const addEvent = (newEvent: EventProps) => {
+    if (events !== null) setEvents([...events, newEvent]);
+  };
+
   function convertIEventsToEventProps(events: IEvent[]): EventProps[] {
     return events.map((event) => {
       return {
@@ -75,156 +75,6 @@ const EventContainer = () => {
     }
   }
 
-  const handleAddEvent = () => {
-    handleShowAddEvent();
-  };
-
-  const handleSaveAdd = () => {
-    handleAdd();
-    setShowModalAddEvent(false);
-  };
-
-  const [formData, setFormData] = useState<EventProps>({
-    date: new Date(), // Provide initial value for date
-    title: '', // Provide initial value for title
-    details: '', // Provide initial value for details
-    image:
-      'https://firebasestorage.googleapis.com/v0/b/pico-7a9d2.appspot.com/o/event-img%2FRobtics.png?alt=media&token=ebd02a49-3e7a-4165-8580-825a2d5a0a5d', // Provide initial value for image
-    onEventDelete: (_id: string) => {}, // Change the parameter type from '_id: string' to 'id: number'
-    onEventEdit: (_event: EventProps) => {},
-    id: '' // Provide initial value for id
-  });
-
-  const event: IEvent = {
-    date: new Date(formData.date),
-    title: formData.title,
-    details: formData.details,
-    imageURL: formData.image,
-    id: ''
-  };
-
-  async function handleAdd() {
-    handleShowAddEvent();
-    const docRef = await eventRepository.create(event);
-    event.id = docRef.id;
-    formData.id = docRef.id;
-    if (file) {
-      await storageService.upload(
-        file,
-        '/event-img/' + docRef.id,
-        setUploadProgress,
-        (_error) => {},
-        () => {
-          const storage = getStorage();
-          const filePath = '/event-img/' + docRef.id;
-          // Get the download URL
-          getDownloadURL(ref(storage, filePath)).then((url) => {
-            event.imageURL = url;
-            formData.image = url;
-            eventRepository.update(docRef.id, event);
-            events?.push(formData);
-            setRender(render === 1 ? 0 : 1);
-          });
-        }
-      );
-    } else {
-      events?.push(formData);
-      setRender(render === 1 ? 0 : 1);
-    }
-  }
-
-  function addWindow() {
-    return (
-      <>
-        <Modal show={showModalAddEvent} onHide={handleCloseAddEvent} animation={false} style={{ display: 'center' }}>
-          <Modal.Header closeButton>
-            <Modal.Title>הוסף אירוע</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>{addForm()}</Modal.Body>
-          <Modal.Footer></Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-
-  function addForm() {
-    const MAX_CHARS_Title = 17; // Set the maximum number of characters allowed
-    const MAX_CHARS_Details = 100; // Set the maximum number of characters allowed
-
-    const handleDetailsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      const value = e.target.value;
-      if (value.length <= MAX_CHARS_Details) {
-        setFormData((prevState) => ({ ...prevState, details: value }));
-      }
-    };
-
-    const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      const value = e.target.value;
-      if (value.length <= MAX_CHARS_Title) {
-        setFormData((prevState) => ({ ...prevState, title: value }));
-      }
-    };
-
-    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData((prevState) => ({ ...prevState, date: e.target.valueAsDate! }));
-    };
-
-    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData((prevState) => ({ ...prevState, image: formData.image }));
-      setFile(e.target.files?.[0] || null); // Provide a default value of null for the file state variable
-    };
-
-    return (
-      <Form onSubmit={handleSaveAdd}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>כותרת</Form.Label>
-          <Form.Control
-            required
-            type="text"
-            placeholder="שם אירוע"
-            onChange={handleTitleChange}
-            maxLength={MAX_CHARS_Title} // Set the maximum length of the textarea
-          />
-          <small>
-            {formData.title.length}/{MAX_CHARS_Title} אותיות
-          </small>{' '}
-          {/* Display the character count */}
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>תאריך</Form.Label>
-          <Form.Control required type="date" placeholder="יום /חודש /שנה" onChange={handleDateChange} />
-        </Form.Group>
-        <Form.Group controlId="formFile" className="mb-3">
-          <Form.Label>העלאת תמונה</Form.Label>
-          <Form.Control type="file" accept="image/*" onChange={handleImageChange} />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>פרטים</Form.Label>
-          <Form.Control
-            required
-            as="textarea"
-            rows={3}
-            placeholder="פרטי האירוע"
-            onChange={handleDetailsChange}
-            maxLength={MAX_CHARS_Details} // Set the maximum length of the textarea
-          />
-          <small>
-            {formData.details.length}/{MAX_CHARS_Details} אותיות
-          </small>{' '}
-          {/* Display the character count */}
-        </Form.Group>
-        <div style={{ display: 'flex', gap: '10px' }}>
-          <Button variant="secondary" onClick={handleCloseAddEvent}>
-            סגור
-          </Button>
-          <Button variant="primary" type="submit">
-            הוסף
-          </Button>
-        </div>
-      </Form>
-    );
-  }
-
   if (events === null) {
     return (
       <Box className="loading-event">
@@ -236,8 +86,7 @@ const EventContainer = () => {
   if (events.length === 0) {
     return (
       <>
-        <EmptyEventCard handleAddEvent={handleAddEvent} />
-        {addWindow()}
+        <EmptyEventCard addEvent={addEvent} />
       </>
     );
   }
@@ -245,9 +94,7 @@ const EventContainer = () => {
   return (
     <div className="events">
       <div className="events-header-button">
-        <Button className="add-button" onClick={handleAddEvent}>
-          <AddIcon />
-        </Button>
+        <AddEvent addEvent={addEvent} />
         <Button className="show-all" onClick={handleAllEvents}>
           {showModalAllEvents ? <div>הצג הכול</div> : <div>הראי פחות</div>}
         </Button>
@@ -292,7 +139,6 @@ const EventContainer = () => {
           ))}
         </div>
       )}
-      {addWindow()}
     </div>
   );
 };
