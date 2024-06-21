93d620f8e9cf023d71aecdc48985dcc9851fcd93
Mosa Salman <mosa.salman.g@gmail.com>
2024-06-21 07:48:18 +0300

    fix: change entity to the proper one.

--- src/events/repository/StudentEventRepository.ts
+++ src/events/repository/StudentEventRepository.ts
@@ -1,7 +1,8 @@
 import { db } from '../../firebase';
 import { BaseRepository } from '../../repositories/BaseRepository';
+import { BriefEvent } from './BriefEvent';
 
-export class StudentEventRepository extends BaseRepository<BriefStudent> {
+export class StudentEventRepository extends BaseRepository<BriefEvent> {
   constructor(id: string) {
     super(db, 'students/' + id + '/events');
   }
