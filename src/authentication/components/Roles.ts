56a8b7b8c2c9156037c502a833f17b519911cfe9
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-01 14:20:49 +0300

    feat: Update role colors in Roles.ts

--- src/authentication/components/Roles.ts
+++ src/authentication/components/Roles.ts
@@ -11,7 +11,7 @@ enum Role {
   Unauthenticated
 }
 
-const RoleNames = {
+const roleNames = {
   [Role.Owner]: 'בעלים',
   [Role.Admin]: 'מנהל',
   [Role.Student]: 'תלמיד',
@@ -19,6 +19,37 @@ const RoleNames = {
   [Role.PreEnrollment]: 'מועמד',
   [Role.Rejected]: 'נדחה',
   [Role.Unauthenticated]: 'לא מאומת'
+};
+
+const roleColors = {
+  [Role.Owner]: {
+    main: '#1B5E20', // Dark Green
+    contrastText: '#FFFFFF' // White
+  },
+  [Role.Admin]: {
+    main: '#0D47A1', // Dark Blue
+    contrastText: '#FFFFFF' // White
+  },
+  [Role.Student]: {
+    main: '#0288D1', // Light Blue
+    contrastText: '#FFFFFF' // White
+  },
+  [Role.Pending]: {
+    main: '#F57F17', // Orange
+    contrastText: '#000000' // Black
+  },
+  [Role.PreEnrollment]: {
+    main: '#FBC02D', // Yellow
+    contrastText: '#000000' // Black
+  },
+  [Role.Rejected]: {
+    main: '#D32F2F', // Red
+    contrastText: '#FFFFFF' // White
+  },
+  [Role.Unauthenticated]: {
+    main: '#9E9E9E', // Gray
+    contrastText: '#000000' // Black
+  }
 };
 
 /**
@@ -36,4 +67,4 @@ enum AuthorizationStatus {
 const ALLOW_AUTHED_ROLES = [Role.Owner, Role.Admin, Role.Student];
 
 export default Role;
-export { ALLOW_AUTHED_ROLES, RoleNames, AuthorizationStatus };
+export { ALLOW_AUTHED_ROLES, roleNames as RoleNames, roleColors as RoleColors, AuthorizationStatus };
