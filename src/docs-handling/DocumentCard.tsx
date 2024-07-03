b1cd7784019ea1b6378e9cdb642a8fe126b1aaf0
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-03 22:58:12 +0300

    feat: Add download docs.

--- src/docs-handling/DocumentCard.tsx
+++ src/docs-handling/DocumentCard.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Card, CardContent, CardActions, Button, Typography, Box, IconButton } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import GetAppIcon from '@mui/icons-material/GetApp';
 import { Delete } from '@mui/icons-material';
+import { StorageServiceContext } from '../storage-service/StorageContext';
+import { DocumentInfo } from './DocumentInfo';
 
 interface DocumentCardProps {
   document: DocumentInfo;
@@ -12,6 +14,8 @@ interface DocumentCardProps {
 
 const DocumentCard: React.FC<DocumentCardProps> = ({ document, onDocumentDelete, onDocumentUpdate }) => {
   const [file, setFile] = useState<File | null>(null);
+
+  const storage = useContext(StorageServiceContext);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
@@ -27,6 +31,12 @@ const DocumentCard: React.FC<DocumentCardProps> = ({ document, onDocumentDelete,
     onDocumentDelete(document.id);
   };
 
+  const handleDownload = () => {
+    storage.download(`documents/${document.id}`, document.filename).then(() => {
+      console.log('Downloaded');
+    });
+  };
+
   return (
     <Card sx={{ maxWidth: 600, margin: '1rem' }}>
       <CardContent>
@@ -39,7 +49,7 @@ const DocumentCard: React.FC<DocumentCardProps> = ({ document, onDocumentDelete,
       </CardContent>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
         <CardActions>
-          <Button variant="contained" color="primary" startIcon={<GetAppIcon />} href={document.downloadLink} download>
+          <Button variant="contained" color="primary" startIcon={<GetAppIcon />} onClick={handleDownload}>
             הורד
           </Button>
           <Button variant="contained" component="label" color="secondary" startIcon={<CloudUploadIcon />}>
