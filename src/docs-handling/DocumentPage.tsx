cd71f68d2739d309ce4b8beb7d570817c9dd1e60
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-04 16:48:22 +0300

    feat: Add DocumentStudentRepository, DocumentRepository, and DocumentsPage components

--- src/docs-handling/DocumentPage.tsx
+++ src/docs-handling/DocumentPage.tsx
@@ -1,12 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Button, Card, CardActions, CardContent } from '@mui/material';
-import { useDocumentInfo } from './DocumentInfoContext';
+import { useDocumentInfoService } from './service/DocumentInfoContext';
 import DocumentCard from './DocumentCard';
 import { Add } from '@mui/icons-material';
 import DocumentFormModal from './DocumentForm';
-import { StorageServiceContext } from '../storage-service/StorageContext';
 import { AuthContext } from '../authentication/services/AuthContext';
-import { DocumentInfo } from './DocumentInfo';
+import { DocumentInfo } from './service/DocumentInfo';
 
 const DocumentsPage: React.FC = () => {
   const [documents, setDocuments] = useState<DocumentInfo[] | undefined>(undefined);
@@ -14,22 +13,24 @@ const DocumentsPage: React.FC = () => {
 
   const handleShow = () => setShow(true);
 
-  const documentRepository = useDocumentInfo();
-  const storage = useContext(StorageServiceContext);
+  const documentInfoService = useDocumentInfoService();
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
-    if (!documentRepository) {
+    if (!documentInfoService) {
       return;
     }
 
-    documentRepository.find().then((docs) => {
-      setDocuments(docs);
-    });
-  }, [documentRepository]);
+    documentInfoService
+      .getDocumentRepository()
+      .find()
+      .then((docs) => {
+        setDocuments(docs);
+      });
+  }, [documentInfoService]);
 
   const handleDocumentAdd = (document: DocumentInfo, file?: File | undefined): Promise<void> => {
-    if (!documentRepository) {
+    if (!documentInfoService) {
       return Promise.resolve();
     }
 
@@ -37,66 +38,62 @@ const DocumentsPage: React.FC = () => {
       return Promise.resolve();
     }
 
-    return documentRepository.create(document).then((doc) => {
-      storage.upload(
-        file,
-        `documents/${doc.id}`,
-        () => {},
-        () => {},
-        () => {
-          document.id = doc.id;
-          setDocuments((prevDocs) => {
-            if (!prevDocs) return [];
-            return [...prevDocs, document];
-          });
-        }
-      );
+    return documentInfoService.addDocument(document, file).then((docRef) => {
+      document.id = docRef.id;
+      setDocuments((prevDocs) => {
+        if (!prevDocs) return [];
+        return [...prevDocs, document];
+      });
     });
   };
 
   const handleDocumentDelete = (documentId: string) => {
-    if (!documentRepository) {
+    if (!documentInfoService) {
       return;
     }
 
-    documentRepository.delete(documentId).then(() => {
-      storage.delete(`documents/${documentId}`).then(() => {
-        setDocuments((prevDocs) => {
-          if (!prevDocs) return [];
-          return prevDocs.filter((doc) => doc.id !== documentId);
-        });
+    documentInfoService.deleteDocument(documentId).then(() => {
+      setDocuments((prevDocs) => {
+        if (!prevDocs) return [];
+        return prevDocs.filter((doc) => doc.id !== documentId);
       });
     });
   };
 
   const handleDocumentUpdate = (document: DocumentInfo, file?: File) => {
-    if (!documentRepository) {
-      return;
+    if (!documentInfoService) {
+      return Promise.resolve();
     }
 
-    documentRepository.update(document.id, document).then(() => {
-      if (file) {
-        storage.upload(
-          file,
-          `documents/${document.id}-${user?.id}`,
-          () => {},
-          () => {},
-          () => {
-            setDocuments((prevDocs) => {
-              if (!prevDocs) return [];
-              return prevDocs.map((doc) => {
-                if (doc.id === document.id) {
-                  return {
-                    ...doc,
-                    downloadLink: `documents/${document.id}-${user?.id}`
-                  };
-                }
-                return doc;
-              });
-            });
+    const id = document.id.toString();
+    return documentInfoService.updateDocument(document, file).then(() => {
+      document.id = id;
+      setDocuments((prevDocs) => {
+        if (!prevDocs) return [];
+        return prevDocs.map((doc) => {
+          if (doc.id === document.id) {
+            return {
+              ...document
+            };
           }
-        );
-      }
+          return doc;
+        });
+      });
+      return;
+    });
+  };
+
+  const handleStudentUpload = (documentId: string, file: File) => {
+    if (!documentInfoService) {
+      return Promise.resolve();
+    }
+
+    if (!user) {
+      return Promise.resolve();
+    }
+
+    return documentInfoService.uploadStudentDocument(user.id, documentId, file).then(() => {
+      console.log('Uploaded');
     });
   };
 
@@ -113,9 +110,10 @@ const DocumentsPage: React.FC = () => {
           {documents?.map((doc) => (
             <DocumentCard
               key={doc.id}
-              document={doc}
+              documentInfo={doc}
               onDocumentDelete={handleDocumentDelete}
               onDocumentUpdate={handleDocumentUpdate}
+              onStudentUpload={handleStudentUpload}
             />
           ))}
         </CardContent>
