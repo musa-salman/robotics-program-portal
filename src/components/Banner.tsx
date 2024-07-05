568305c51c2f4593d0dbba61839179630fecbbcc
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-05 09:52:05 +0300

    feat: Add PictureAsPdfIcon to Banner component for displaying documents link

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,17 +4,19 @@ import BannerButton from './BannerButton';
 import BookIcon from '@mui/icons-material/Book';
 import EventIcon from '@mui/icons-material/Event';
 import PeopleIcon from '@mui/icons-material/People';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 import './Banner.css';
 
 const USER_LINKS = [
   { icon: <BookIcon />, label: 'חומרי לימוד', path: '/study-materials' },
-  { icon: <EventIcon />, label: 'אירועים', path: '/events' }
+  { icon: <EventIcon />, label: 'אירועים', path: '/events' },
+  { icon: <PictureAsPdfIcon />, label: 'מסמכים', path: '/documents' }
 ];
 
 const ADMIN_LINKS = [
   { icon: <PeopleIcon />, label: 'ניהול תלמדים', path: '/students-management' },
   { icon: <PeopleIcon />, label: 'ניהול נרשמים', path: '/registers-management' },
-  { icon: <PeopleIcon />, label: 'ניהול משתמשים', path: '/users-management' }
+  { icon: <PeopleIcon />, label: 'ניהול משתמשים', path: '/users' }
 ];
 
 const Banner: React.FC = () => {
