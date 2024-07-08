07f62022c60df4207a4fa7e14827ff1d2a102140
Haddel456 <hadeelm046@gmail.com>
2024-07-08 23:02:27 +0300

    finish : the splash screen

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -21,14 +21,39 @@ function SplashScreen() {
           <img src="rr.png" style={{ width: '15rem' }} />
         </div>
         <div className="contant">
-          <Typography variant="h6" style={{ marginBottom: '10px' }}>
-            Albert Einstein Quote
-          </Typography>
+          <div>
+            <Grid container spacing={4} style={{ marginBottom: '10px', margin: '10px' }}>
+              <Grid item xs={14} sm={6}>
+                <Box className="box">
+                  <Typography variant="h4" style={{ marginBottom: '10px', margin: '10px' }}>
+                    ברוכים הבאים לאתר המגמה למכטרוניקה
+                  </Typography>
 
-          <Typography variant="body1" style={{ alignItems: 'end' }}>
-            "הדבר היפה ביותר שאנו יכולים לחוות הוא המסתורין , הוא המקור של כל האומנות והמדע"- אלברט איינשטיין
-          </Typography>
+                  <Typography variant="body1" style={{ marginBottom: '10px', margin: '10px' }}>
+                    המגמה למכטרוניקה עברה למתחם חדש! אנחנו נרגשים להודיע על פתיחת המתחם החדש שלנו בבית הספר קשת רא"ם
+                    בארמון הנציב. הושקע תקציב רב בהצטיידות המתחם ובצוות מורים ומנטורים מעולים
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </div>
+
+          <div>
+            <Grid container spacing={4} style={{ marginBottom: '10px', margin: '10px' }}>
+              <Grid item xs={12} sm={6}>
+                <Box className="box" p={2} border={1} borderRadius={2} boxShadow={2}>
+                  <Typography variant="body1" gutterBottom style={{ alignItems: 'end' }}>
+                    "הדבר היפה ביותר שאנו יכולים לחוות הוא המסתורין , הוא המקור של כל האומנות והמדע"
+                  </Typography>
+                  <Typography variant="subtitle1" className="name" align="left">
+                    -אלברט איינשטיין -
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </div>
         </div>
+
         <Grid container justifyContent="left" className="foo">
           <Grid item>
             <Link href="https://www.facebook.com/teamstreak7067/" target="_blank" rel="noopener noreferrer">
