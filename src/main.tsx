87e81bb281e22151ec45a10df42f05c5d2955c22
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-09 17:42:22 +0300

    feat: Update theme palette in main.tsx to include dark mode and new color scheme

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,43 @@ const cacheRtl = createCache({
 
 const theme = createTheme({
   palette: {
-    mode: 'dark'
+    mode: 'dark',
+    primary: {
+      main: '#F2542D'
+    },
+    secondary: {
+      main: '#f48fb1'
+    },
+    error: {
+      main: '#f44336'
+    },
+    background: {
+      default: '#3f3f3f',
+      paper: '#082032'
+    },
+    text: {
+      primary: '#fff',
+      secondary: '#aaa',
+      disabled: '#666'
+    },
+    divider: '#333',
+    action: {
+      active: '#fff',
+      hover: '#555',
+      selected: '#666',
+      disabled: '#333',
+      disabledBackground: '#555',
+      focus: '#777'
+    },
+    success: {
+      main: '#4caf50'
+    },
+    warning: {
+      main: '#ff9800'
+    },
+    info: {
+      main: '#2196f3'
+    }
   },
   components: {
     MuiPagination: {
