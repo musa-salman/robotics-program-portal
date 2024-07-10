53d2470eca19e593ff69df7054009bc29bbf171c
Anas_Jamjoum <anas13672@gmail.com>
2024-07-10 21:06:54 +0300

    refactor: Remove unused CSS styles and files

--- src/events/AddEvent.tsx
+++ src/events/AddEvent.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState } from 'react';
-// import { Button, Modal } from 'react-bootstrap';
 import { EventProps } from './EventCard';
 import { IEvent } from './repository/Event';
 import { useEventService } from './repository/EventContext';
