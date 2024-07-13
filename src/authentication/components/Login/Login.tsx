4e4f0249571f3b1a2fc87c9abc9e345cd3e4a3ee
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-14 02:09:28 +0300

    feat: mock data

--- src/authentication/components/Login/Login.tsx
+++ src/authentication/components/Login/Login.tsx
@@ -2,23 +2,30 @@ import React, { useState } from 'react';
 import { Button } from '@mui/material';
 import { useAuth } from '../../services/useAuth';
 import { Login } from '@mui/icons-material';
+import FeedbackSnackbar, { FeedbackMessage } from '../../../components/snackbar/SnackBar';
 
 const LoginButton: React.FC = () => {
   const { authService } = useAuth();
-  const [error, setError] = useState('');
+  const [message, setMessage] = useState<FeedbackMessage | null>(null);
+  const [buildNumber, setBuildNumber] = useState<number>(0);
   const [loading, setLoading] = useState(false);
 
+  const showMessage = (message: FeedbackMessage) => {
+    setMessage(message);
+    setBuildNumber(buildNumber + 1);
+  };
+
   const handleLogin = () => {
-    setError('');
     setLoading(true);
     authService
       .loginWithGoogle()
       .then(() => {
+        showMessage({ message: 'התחברת בהצלחה', variant: 'success' });
         const redirectTo = '/';
         window.location.href = redirectTo;
       })
       .catch(() => {
-        setError('כניסה נכשלה, נסה שוב.');
+        showMessage({ message: 'שגיאה בהתחברות', variant: 'error' });
       })
       .finally(() => {
         setLoading(false);
@@ -26,9 +33,12 @@ const LoginButton: React.FC = () => {
   };
 
   return (
-    <Button variant="contained" color="primary" onClick={handleLogin} startIcon={<Login />} disabled={loading}>
-      {loading ? 'מתחבר...' : 'התחברות'}
-    </Button>
+    <>
+      {message && <FeedbackSnackbar key={buildNumber} feedBackMessage={message} />}
+      <Button variant="contained" color="primary" onClick={handleLogin} startIcon={<Login />} disabled={loading}>
+        {loading ? 'מתחבר...' : 'התחברות'}
+      </Button>
+    </>
   );
 };
 
