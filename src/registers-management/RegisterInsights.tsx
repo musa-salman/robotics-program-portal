7f77bac934be31820ca9e4fee565d588a24c72e4
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-15 14:53:45 +0300

    feat: Update RegisterInsights component to use random values for data

--- src/registers-management/RegisterInsights.tsx
+++ src/registers-management/RegisterInsights.tsx
@@ -1,5 +1,7 @@
 import { PieChart } from '@mui/x-charts';
-import { InsightData } from '../Insights/InsightPage';
+import { InsightData } from '../insights/InsightPage';
+
+const getRandomValue = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const registerInsightList: InsightData = {
   title: 'תובנות על נרשמים',
@@ -12,11 +14,11 @@ const registerInsightList: InsightData = {
             series={[
               {
                 data: [
-                  { id: 0, value: 10, label: 'בית הספר' },
-                  { id: 1, value: 15, label: 'עלון מנח"י' },
-                  { id: 2, value: 20, label: 'קרוב משפחה שלמד במגמה' },
-                  { id: 3, value: 25, label: 'חיפוש עצמי באינטרנט' },
-                  { id: 4, value: 30, label: 'אחרת' }
+                  { id: 0, value: getRandomValue(5, 20), label: 'בית הספר' },
+                  { id: 1, value: getRandomValue(10, 25), label: 'עלון מנח"י' },
+                  { id: 2, value: getRandomValue(15, 30), label: 'קרוב משפחה שלמד במגמה' },
+                  { id: 3, value: getRandomValue(20, 35), label: 'חיפוש עצמי באינטרנט' },
+                  { id: 4, value: getRandomValue(25, 40), label: 'אחרת' }
                 ],
                 highlightScope: { faded: 'global', highlighted: 'item' },
                 faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' }
@@ -35,10 +37,10 @@ const registerInsightList: InsightData = {
             series={[
               {
                 data: [
-                  { id: 0, value: 10, label: '3' },
-                  { id: 1, value: 15, label: '4' },
-                  { id: 2, value: 20, label: '5' },
-                  { id: 3, value: 25, label: 'אחרת' }
+                  { id: 0, value: getRandomValue(5, 20), label: '3' },
+                  { id: 1, value: getRandomValue(10, 25), label: '4' },
+                  { id: 2, value: getRandomValue(15, 30), label: '5' },
+                  { id: 3, value: getRandomValue(20, 35), label: 'אחרת' }
                 ],
                 highlightScope: { faded: 'global', highlighted: 'item' },
                 faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' }
@@ -57,10 +59,10 @@ const registerInsightList: InsightData = {
             series={[
               {
                 data: [
-                  { id: 0, value: 10, label: 'מכטרוניקה 5 יח"ל' },
-                  { id: 1, value: 15, label: 'מכטרוניקה 10 יח"ל' },
-                  { id: 2, value: 20, label: 'עדיין לא ידוע' },
-                  { id: 3, value: 25, label: 'אחרת' }
+                  { id: 0, value: getRandomValue(5, 20), label: 'מכטרוניקה 5 יח"ל' },
+                  { id: 1, value: getRandomValue(10, 25), label: 'מכטרוניקה 10 יח"ל' },
+                  { id: 2, value: getRandomValue(15, 30), label: 'עדיין לא ידוע' },
+                  { id: 3, value: getRandomValue(20, 35), label: 'אחרת' }
                 ],
                 highlightScope: { faded: 'global', highlighted: 'item' },
                 faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' }
