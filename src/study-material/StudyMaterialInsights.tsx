f5f8c75e9f76c250682d7b81a9e00fcaefd5802f
Mosa Salman <mosa.salman.g@gmail.com>
2024-07-15 14:52:36 +0300

    Refactor StudyMaterialInsights component to add charts for insights

---
+++ src/study-material/StudyMaterialInsights.tsx
@@ -0,0 +1,156 @@
+import { PieChart, BarChart, LineChart } from '@mui/x-charts';
+import { InsightData } from '../insights/InsightPage';
+import { StudyMaterial } from './repository/StudyMaterial';
+import { Category } from './repository/Category';
+
+const categories: Category[] = [
+  { id: '1', category: 'מתמטיקה' },
+  { id: '2', category: 'מדע' },
+  { id: '3', category: 'היסטוריה' }
+];
+
+const studyMaterials: StudyMaterial[] = [
+  {
+    id: '1',
+    filename: 'algebra.pdf',
+    category: 'מתמטיקה',
+    title: 'יסודות האלגברה',
+    description: 'מבוא לאלגברה',
+    date: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 5)
+  },
+  {
+    id: '2',
+    filename: 'biology.pdf',
+    category: 'מדע',
+    title: 'ביולוגיה 101',
+    description: 'מושגי ביולוגיה בסיסיים',
+    date: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 3)
+  },
+  {
+    id: '3',
+    filename: 'world-history.pdf',
+    category: 'היסטוריה',
+    title: 'היסטוריה עולמית',
+    description: 'סקירה של היסטוריה עולמית',
+    date: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 2)
+  },
+  {
+    id: '4',
+    filename: 'geometry.pdf',
+    category: 'מתמטיקה',
+    title: 'גיאומטריה לתלמידי חטיבה',
+    description: 'מבוא לגיאומטריה',
+    date: new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate() - 1)
+  },
+  {
+    id: '5',
+    filename: 'chemistry.pdf',
+    category: 'מדע',
+    title: 'כימיה לתלמידי תיכון',
+    description: 'מבוא לכימיה',
+    date: new Date()
+  },
+  {
+    id: '6',
+    filename: 'ancient-history.pdf',
+    category: 'היסטוריה',
+    title: 'היסטוריה עתיקה',
+    description: 'סקירה של ההיסטוריה העתיקה',
+    date: new Date()
+  }
+];
+
+const studyMaterialInsights: InsightData = {
+  title: 'תובנות על חומרי לימוד',
+  insights: [
+    {
+      question: 'איך מחולקים חומרי הלימוד לפי קטגוריות?',
+      generateGraph: () => {
+        const categoryCount = categories.map((category) => ({
+          id: category.id,
+          value: studyMaterials.filter((material) => material.category === category.category).length,
+          label: category.category
+        }));
+
+        return (
+          <PieChart
+            series={[
+              {
+                data: categoryCount,
+                highlightScope: { faded: 'global', highlighted: 'item' },
+                faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' }
+              }
+            ]}
+            height={200}
+          />
+        );
+      }
+    },
+    {
+      question: 'כיצד השתנו ההוספות של חומרי לימוד לאורך זמן?',
+      generateGraph: () => {
+        const studyMaterialCountByDate = studyMaterials.reduce(
+          (acc, material) => {
+            const date = material.date.getTime();
+            acc[date] = acc[date] ? acc[date] + 1 : 1;
+            return acc;
+          },
+          {} as Record<string, number>
+        );
+
+        console.log(studyMaterialCountByDate);
+        return (
+          <LineChart
+            xAxis={[
+              {
+                data: Object.keys(studyMaterialCountByDate),
+                label: 'תאריך',
+                axisId: 'x',
+                scaleType: 'time'
+              }
+            ]}
+            series={[
+              {
+                data: Object.entries(studyMaterialCountByDate).map(([_, count]) => count),
+                label: 'הוספות חומרי לימוד'
+              }
+            ]}
+            height={200}
+          />
+        );
+      }
+    },
+    {
+      question: 'כמות חומרי הלימוד בכל קטגוריה?',
+      generateGraph: () => {
+        const studyMaterialCountByCategory = categories.map((category) => ({
+          id: category.id,
+          value: studyMaterials.filter((material) => material.category === category.category).length,
+          label: category.category
+        }));
+
+        return (
+          <BarChart
+            xAxis={[
+              {
+                data: studyMaterialCountByCategory.map((item) => item.label),
+                label: 'קטגוריה',
+                scaleType: 'band',
+                axisId: 'x'
+              }
+            ]}
+            series={[
+              {
+                data: studyMaterialCountByCategory.map((item) => item.value),
+                label: 'כמות חומרי הלימוד'
+              }
+            ]}
+            height={200}
+          />
+        );
+      }
+    }
+  ]
+};
+
+export default studyMaterialInsights;
