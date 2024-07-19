90b21857ab8401b82385ba8db12a84ae9a6ff485
Anas_Jamjoum <anas13672@gmail.com>
2024-07-19 22:56:37 +0300

    refactor: Rearrange buttons in StudyMaterialContainer for better user experience

--- src/study-material/StudyMaterialContainer.tsx
+++ src/study-material/StudyMaterialContainer.tsx
@@ -167,11 +167,11 @@ function StudyMaterialContainer() {
           <div className="btn-search">
             <RoleBasedAccessControl allowedRoles={[Role.Admin, Role.Owner]} unauthorizedAuthenticatedComponent={<></>}>
               <div className="btns">
-                <Button variant="outlined" aria-label="edit" onClick={handleShowEdit}>
-                  <SettingsIcon />
-                </Button>
                 <Button variant="contained" aria-label="add" onClick={handleShow}>
                   <AddIcon />
+                </Button>
+                <Button variant="outlined" aria-label="edit" onClick={handleShowEdit}>
+                  <SettingsIcon />
                 </Button>
               </div>
             </RoleBasedAccessControl>
