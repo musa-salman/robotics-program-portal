3a115c434f08145ab3d9e08a65fdcb4a63e3ab69
ertugrul2002 <mohamaddkdk2002@gmail.com>
2024-07-20 20:13:44 +0300

    feat:fix

--- src/events/EventInsights.tsx
+++ src/events/EventInsights.tsx
@@ -56,127 +56,127 @@ const eventInsights: InsightData = {
         );
       }
     },
-    {
-      question: 'כמה תלמידים נרשמו לכל אירוע?',
-      generateGraph: () => {
-        const eventService = useEventService();
-        const [eventMetrics, setEventMetrics] = useState<Record<string, IEvent & { count: number }> | null>(null);
-        const [selectedEvents, setSelectedEvents] = useState<string[]>([]);
-        const [startDate, setStartDate] = useState<Moment | null>(null);
-        const [endDate, setEndDate] = useState<Moment | null>(null);
+    // {
+    //   question: 'כמה תלמידים נרשמו לכל אירוע?',
+    //   generateGraph: () => {
+    //     const eventService = useEventService();
+    //     const [eventMetrics, setEventMetrics] = useState<Record<string, IEvent & { count: number }> | null>(null);
+    //     const [selectedEvents, setSelectedEvents] = useState<string[]>([]);
+    //     const [startDate, setStartDate] = useState<Moment | null>(null);
+    //     const [endDate, setEndDate] = useState<Moment | null>(null);
 
-        useEffect(() => {
-          if (eventMetrics === null) {
-            eventService
-              .aggregateEventRegistrations()
-              .then((data) => {
-                setSelectedEvents(Object.keys(data));
-                setEventMetrics(data);
-              })
-              .catch((error) => {
-                console.error('Error fetching event metrics:', error);
-              });
-          }
-        }, [eventMetrics, eventService]);
-        if (eventMetrics === null) {
-          return <div>Loading...</div>;
-        }
+    //     useEffect(() => {
+    //       if (eventMetrics === null) {
+    //         eventService
+    //           .aggregateEventRegistrations()
+    //           .then((data) => {
+    //             setSelectedEvents(Object.keys(data));
+    //             setEventMetrics(data);
+    //           })
+    //           .catch((error) => {
+    //             console.error('Error fetching event metrics:', error);
+    //           });
+    //       }
+    //     }, [eventMetrics, eventService]);
+    //     if (eventMetrics === null) {
+    //       return <div>Loading...</div>;
+    //     }
 
-        // const handleSelectedEvents = (event: any) => {
-        //   event.preventDefault();
-        //   event.stopPropagation();
-        //   setSelectedEvents(event.target.value as string[]);
-        // };
+    //     // const handleSelectedEvents = (event: any) => {
+    //     //   event.preventDefault();
+    //     //   event.stopPropagation();
+    //     //   setSelectedEvents(event.target.value as string[]);
+    //     // };
 
-        // const handleDateRange = (date: Moment | null, type: 'start' | 'end') => {
-        //   if (type === 'start') {
-        //     setStartDate(date);
-        //   } else {
-        //     setEndDate(date);
-        //   }
+    //     // const handleDateRange = (date: Moment | null, type: 'start' | 'end') => {
+    //     //   if (type === 'start') {
+    //     //     setStartDate(date);
+    //     //   } else {
+    //     //     setEndDate(date);
+    //     //   }
 
-        //   if (startDate !== null && endDate !== null&& eventMetrics !== null) {
-        //     console.log('eventMetrics', eventMetrics);
-        //     setSelectedEvents(
-        //       Object.keys(eventMetrics).filter((event) => {
-        //         const eventDate = eventMetrics[event].startDate;
-        //         return eventDate >= startDate.toDate() && eventDate <= endDate.toDate();
-        //       })
-        //     );
-        //   }
-        // };
+    //     //   if (startDate !== null && endDate !== null&& eventMetrics !== null) {
+    //     //     console.log('eventMetrics', eventMetrics);
+    //     //     setSelectedEvents(
+    //     //       Object.keys(eventMetrics).filter((event) => {
+    //     //         const eventDate = eventMetrics[event].startDate;
+    //     //         return eventDate >= startDate.toDate() && eventDate <= endDate.toDate();
+    //     //       })
+    //     //     );
+    //     //   }
+    //     // };
 
-        return (
-          <>
-            {/* <FormControl fullWidth sx={{ m: 1, display: 'flex', flexDirection: 'row', gap: '10px' }}>
-              <InputLabel id="demo-multiple-checkbox-label">בחר אירועים</InputLabel>
-              <Select
-                sx={{
-                  width: '50%'
-                }}
-                labelId="demo-multiple-checkbox-label"
-                id="demo-multiple-checkbox"
-                multiple
-                value={selectedEvents}
-                onChange={(event) => handleSelectedEvents(event)}
-                renderValue={(selected) => selected.join(', ')}>
-                {eventMetrics &&
-                  Object.keys(eventMetrics).map((name) => (
-                    <MenuItem key={name} value={name}>
-                      <Checkbox checked={selectedEvents.indexOf(name) > -1} />
-                      <ListItemText primary={name} />
-                    </MenuItem>
-                  ))}
-              </Select>
-              <LocalizationProvider adapterLocale={'he'} dateAdapter={AdapterMoment}>
-                <DatePicker
-                  label="תאריך התחלה"
-                  onChange={(newValue) => {
-                    handleDateRange(newValue, 'start');
-                  }}
-                />
-                <DatePicker
-                  label="תאריך סיום"
-                  onChange={(newValue) => {
-                    handleDateRange(newValue, 'end');
-                  }}
-                />
-              </LocalizationProvider>
-            </FormControl>
-            <BarChart
-              xAxis={[
-                {
-                  data: selectedEvents,
-                  label: 'אירוע',
-                  axisId: 'x',
-                  scaleType: 'band'
-                }
-              ]}
-              series={[
-                {
-                  data: selectedEvents.map((event) => (eventMetrics ? eventMetrics[event].count : 0))
-                }
-              ]}
-              height={200}
-            /> */}
-            <PieChart
-              series={[
-                {
-                  data: [
-                    { id: 'event1', value: 5, label: 'אירוע 1' },
-                    { id: 'event2', value: 10, label: 'אירוע 2' },
-                    { id: 'event3', value: 20, label: 'אירוע 3' }
-                  ],
-                  highlightScope: { faded: 'global', highlighted: 'item' },
-                  faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' }
-                }
-              ]}
-              height={200}
-            />
-          </>
-        );
-      }
-    }
+    //     return (
+    //       <>
+    //         {/* <FormControl fullWidth sx={{ m: 1, display: 'flex', flexDirection: 'row', gap: '10px' }}>
+    //           <InputLabel id="demo-multiple-checkbox-label">בחר אירועים</InputLabel>
+    //           <Select
+    //             sx={{
+    //               width: '50%'
+    //             }}
+    //             labelId="demo-multiple-checkbox-label"
+    //             id="demo-multiple-checkbox"
+    //             multiple
+    //             value={selectedEvents}
+    //             onChange={(event) => handleSelectedEvents(event)}
+    //             renderValue={(selected) => selected.join(', ')}>
+    //             {eventMetrics &&
+    //               Object.keys(eventMetrics).map((name) => (
+    //                 <MenuItem key={name} value={name}>
+    //                   <Checkbox checked={selectedEvents.indexOf(name) > -1} />
+    //                   <ListItemText primary={name} />
+    //                 </MenuItem>
+    //               ))}
+    //           </Select>
+    //           <LocalizationProvider adapterLocale={'he'} dateAdapter={AdapterMoment}>
+    //             <DatePicker
+    //               label="תאריך התחלה"
+    //               onChange={(newValue) => {
+    //                 handleDateRange(newValue, 'start');
+    //               }}
+    //             />
+    //             <DatePicker
+    //               label="תאריך סיום"
+    //               onChange={(newValue) => {
+    //                 handleDateRange(newValue, 'end');
+    //               }}
+    //             />
+    //           </LocalizationProvider>
+    //         </FormControl>
+    //         <BarChart
+    //           xAxis={[
+    //             {
+    //               data: selectedEvents,
+    //               label: 'אירוע',
+    //               axisId: 'x',
+    //               scaleType: 'band'
+    //             }
+    //           ]}
+    //           series={[
+    //             {
+    //               data: selectedEvents.map((event) => (eventMetrics ? eventMetrics[event].count : 0))
+    //             }
+    //           ]}
+    //           height={200}
+    //         /> */}
+    //         <PieChart
+    //           series={[
+    //             {
+    //               data: [
+    //                 { id: 'event1', value: 5, label: 'אירוע 1' },
+    //                 { id: 'event2', value: 10, label: 'אירוע 2' },
+    //                 { id: 'event3', value: 20, label: 'אירוע 3' }
+    //               ],
+    //               highlightScope: { faded: 'global', highlighted: 'item' },
+    //               faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' }
+    //             }
+    //           ]}
+    //           height={200}
+    //         />
+    //       </>
+    //     );
+    //   }
+    // }
 
     // {
     //   question: 'כיצד התפתחו ההרשמות לאירועים לאורך הזמן?',
--- src/study-material/StudyMaterialInsights.tsx
+++ src/study-material/StudyMaterialInsights.tsx
@@ -134,72 +134,72 @@ const studyMaterialInsights: InsightData = {
         );
       }
     },
-    {
-      question: 'כיצד השתנו ההוספות של חומרי לימוד לאורך זמן?',
-      generateGraph: () => {
-        const [studyMaterials, setStudyMaterials] = useState<StudyMaterial[] | null>(null);
-        const [categories, setCategories] = useState<Category[] | null>(null);
-        const [studyMaterialCountByDate, setStudyMaterialCountByDate] = useState<Record<string, number>>();
-        const studyMaterialManagement = useMaterialService();
+    // {
+    //   question: 'כיצד השתנו ההוספות של חומרי לימוד לאורך זמן?',
+    //   generateGraph: () => {
+    //     const [studyMaterials, setStudyMaterials] = useState<StudyMaterial[] | null>(null);
+    //     const [categories, setCategories] = useState<Category[] | null>(null);
+    //     const [studyMaterialCountByDate, setStudyMaterialCountByDate] = useState<Record<string, number>>();
+    //     const studyMaterialManagement = useMaterialService();
 
-        useEffect(() => {
-          if (studyMaterials === null) {
-            studyMaterialManagement.studyMaterialRepository
-              .find()
-              .then((data) => {
-                setStudyMaterials(data);
-              })
-              .catch((error) => {
-                console.log('error while git studyMaterial');
-              });
-          }
-          if (studyMaterials !== null) {
-            const studyMaterialCount = studyMaterials.reduce(
-              (acc, material) => {
-                const date = new Date(material.date);
-                const year = date.getUTCFullYear();
-                const month = String(date.getUTCMonth() + 1).padStart(2, '0');
-                const day = String(date.getUTCDate()).padStart(2, '0');
-                const formattedDate = `${year}-${month}-${day}`;
-                console.log(formatDate);
-                acc[formattedDate] = acc[formattedDate] ? acc[formattedDate] + 1 : 1;
-                return acc;
-              },
-              {} as Record<string, number>
-            );
-            setStudyMaterialCountByDate(studyMaterialCount);
-          }
-        }, [studyMaterials]);
-        if (studyMaterials === null) {
-          return <div>Loading...</div>;
-        }
+    //     useEffect(() => {
+    //       if (studyMaterials === null) {
+    //         studyMaterialManagement.studyMaterialRepository
+    //           .find()
+    //           .then((data) => {
+    //             setStudyMaterials(data);
+    //           })
+    //           .catch((error) => {
+    //             console.log('error while git studyMaterial');
+    //           });
+    //       }
+    //       if (studyMaterials !== null) {
+    //         const studyMaterialCount = studyMaterials.reduce(
+    //           (acc, material) => {
+    //             const date = new Date(material.date);
+    //             const year = date.getUTCFullYear();
+    //             const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+    //             const day = String(date.getUTCDate()).padStart(2, '0');
+    //             const formattedDate = `${year}-${month}-${day}`;
+    //             console.log(formatDate);
+    //             acc[formattedDate] = acc[formattedDate] ? acc[formattedDate] + 1 : 1;
+    //             return acc;
+    //           },
+    //           {} as Record<string, number>
+    //         );
+    //         setStudyMaterialCountByDate(studyMaterialCount);
+    //       }
+    //     }, [studyMaterials]);
+    //     if (studyMaterials === null) {
+    //       return <div>Loading...</div>;
+    //     }
 
-        console.log('f', studyMaterialCountByDate);
-        return (
-          <>
-            {studyMaterialCountByDate !== undefined && (
-              <LineChart
-                xAxis={[
-                  {
-                    data: Object.keys(studyMaterialCountByDate),
-                    label: 'תאריך',
-                    axisId: 'x',
-                    scaleType: 'time'
-                  }
-                ]}
-                series={[
-                  {
-                    data: Object.entries(studyMaterialCountByDate).map(([_, count]) => count),
-                    label: 'הוספות חומרי לימוד'
-                  }
-                ]}
-                height={200}
-              />
-            )}
-          </>
-        );
-      }
-    }
+    //     console.log('f', studyMaterialCountByDate);
+    //     return (
+    //       <>
+    //         {studyMaterialCountByDate !== undefined && (
+    //           <LineChart
+    //             xAxis={[
+    //               {
+    //                 data: Object.keys(studyMaterialCountByDate),
+    //                 label: 'תאריך',
+    //                 axisId: 'x',
+    //                 scaleType: 'time'
+    //               }
+    //             ]}
+    //             series={[
+    //               {
+    //                 data: Object.entries(studyMaterialCountByDate).map(([_, count]) => count),
+    //                 label: 'הוספות חומרי לימוד'
+    //               }
+    //             ]}
+    //             height={200}
+    //           />
+    //         )}
+    //       </>
+    //     );
+    //   }
+    // }
     // {
     //   question: 'כמות חומרי הלימוד בכל קטגוריה?',
     //   generateGraph: () => {
