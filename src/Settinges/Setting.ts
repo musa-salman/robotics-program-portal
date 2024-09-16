daee981d9341a1041d74927bd3543d9b9ee34d2d
Mosa Salman <mosa.salman.g@gmail.com>
2024-09-16 12:07:26 +0300

    refactor: Remove unused Setting-related files and code

--- src/Settinges/Setting.ts
+++
@@ -1,4 +0,0 @@
-export interface Setting {
-  id: string;
-  gptApi: string;
-}
--- src/Settinges/SettingRepository.ts
+++
@@ -1,9 +0,0 @@
-import { db } from '../firebase';
-import { BaseRepository } from '../repositories/BaseRepository';
-import { Setting } from './Setting';
-
-export class SettingRepository extends BaseRepository<Setting> {
-  constructor() {
-    super(db, 'gptApi');
-  }
-}
